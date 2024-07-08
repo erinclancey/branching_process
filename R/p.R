7e50a5e976f814a83c2fd80e385a5e47587f9f33
Andrew Redd <andrew.redd@hsc.utah.edu>
2024-07-08 13:57:10 -0600

    Documentation for `p()`. Also fixes for check() to run successfully.

--- R/p.R
+++ R/p.R
@@ -10,15 +10,18 @@
 # using mu = R and size = k in R's "NegBinomial", e.g. dnbinom(x, mu=R, size=k) would give the density,
 # i.e. the probability of exactly x transmissions from one individual
 
-# p is the probability of y total transmission directly from x independent infected individuals:
 
 
-
-# Example:
-#
-# p(5, 0:15, R=0.2, k=0.1)
-#
-# With 5 individuals in generation n and negative binomial offspring distribution with mean R=0.2
-# and dispersion k=0.1, gives the probability that generation n+1 has a total of 0 to 15 individuals 
-
+#' Probability of y total transmission directly from x independent infected individuals
+#'
+#' @param x number of infected individuals in generation n
+#' @param y number of total transmissions in generation n+1
+#' @param R mean of negative binomial offspring distribution
+#' @param k dispersion of negative binomial offspring distribution
+#'
+#' @examples
+#' # With 5 individuals in this generation, what is the probability of
+#' # 0 to 15 transmissions in the next generation?
+#' p(5, 0:15, R=0.2, k=0.1)
+#' @export
 p <- function(x,y,R,k) exp(lgamma(k*x+y)-lgamma(k*x)-lgamma(y+1)+y*log(R/k)-(k*x+y)*log(1+R/k))
