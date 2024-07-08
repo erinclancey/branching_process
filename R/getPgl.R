a13567193b68a0a50821eba5503681ced9fbbe59
George Vega Yon <g.vegayon@gmail.com>
2024-07-08 13:49:16 -0600

    Adding a function to namespace

--- R/getPgl.R
+++ R/getPgl.R
@@ -1,9 +1,16 @@
-# pgl[g] is the probability that one initial case leads to an outbreak lasting less than g generations of transmission
-# getPgl calculates pgl[g] for all g from 1 to gMax
-
-getPgl <- function(gMax,R,k){
+#' getPgl calculates pgl[g] for all g from 1 to gMax
+#'
+#' pgl[g] is the probability that one initial case leads to an outbreak lasting
+#' less than g generations of transmission.
+#'
+#' @param gMax maximum number of generations.
+#' @param R reproduction number.
+#' @param k dispersion parameter.
+#' @author Damon Toth
+#' @export
+getPgl <- function(gMax,R,k) {
   pgl <- rep(0,gMax)
   pgl[1] <- (1+R/k)^(-k)
-  if(gMax > 1) for(g in 2:gMax) pgl[g] <- (1+R/k*(1-pgl[g-1]))^(-k)	
+  if(gMax > 1) for(g in 2:gMax) pgl[g] <- (1+R/k*(1-pgl[g-1]))^(-k)
   pgl
 }
