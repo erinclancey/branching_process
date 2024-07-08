61472dd988b912ff1eb0faf1030b11a3ad456727
Andrew Redd <andrew.redd@hsc.utah.edu>
2024-07-08 14:12:32 -0600

    Expanded documentation for package.

--- R/p.R
+++ R/p.R
@@ -1,17 +1,3 @@
-# This package provides functions that quantify infectious disease outbreaks using a branching process,
-# a stochastic process in which each individual in generation n produces a random number of individuals
-# in generation n+1, continuing for some number of generations or until there are no individuals
-# remaining (stochastic extinction).
-
-# The random number of next-generation individuals produced by each individual is drawn from the
-# offspring distribution, a discrete probability distribution with non-negative range. To model
-# infectious disease outbreaks, it is common to use a negative binomial offspring distribution,
-# parameterized by the mean R and dispersion parameter k. This parameterization is equivalent to
-# using mu = R and size = k in R's "NegBinomial", e.g. dnbinom(x, mu=R, size=k) would give the density,
-# i.e. the probability of exactly x transmissions from one individual
-
-
-
 #' Probability of y total transmission directly from x independent infected individuals
 #'
 #' @param x number of infected individuals in generation n
