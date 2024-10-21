1ede2d11db95d45c1422de6c810abdd0f215bdd4
damontoth <Damon.Toth@hsc.utah.edu>
2024-10-21 15:24:45 -0600

    Changed name of p.R to pNextGen.R

---
+++ R/pNextGen.R
@@ -0,0 +1,13 @@
+#' Probability of y total transmission directly from x independent infected individuals
+#'
+#' @param x number of infected individuals in generation n
+#' @param y number of total transmissions in generation n+1
+#' @param R mean of negative binomial offspring distribution
+#' @param k dispersion of negative binomial offspring distribution
+#'
+#' @examples
+#' # With 5 individuals in this generation, what is the probability of
+#' # 0 to 15 transmissions in the next generation?
+#' pNextGen(5, 0:15, R=0.2, k=0.1)
+#' @export
+pNextGen <- function(x,y,R,k) exp(lgamma(k*x+y)-lgamma(k*x)-lgamma(y+1)+y*log(R/k)-(k*x+y)*log(1+R/k))
