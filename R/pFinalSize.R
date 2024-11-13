7c39d15d74d2bb04f1d0107cc1ce684e56836b49
damontoth <Damon.Toth@hsc.utah.edu>
2024-11-13 16:24:16 -0700

    Added output documentation for pFinalSize #6

--- R/pFinalSize.R
+++ R/pFinalSize.R
@@ -1,17 +1,14 @@
-#' Probability of outbreak extinction
-#'
-#' qAny is the probability that n initial cases lead to an extinguished outbreak
-#' of total size j after any number of transmission generations (j includes the
-#' n initial cases)
+#' Final outbreak size probability
 #'
 #' @param n number of initial cases in generation 0
 #' @param j total outbreak size (>= n).
 #' @param R mean of negative binomial offspring distribution
 #' @param k dispersion of negative binomial offspring distribution
+#' @returns The final size probability
 #' @examples
 #' # With 5 initial individuals and negative binomial offspring distribution with mean R=0.2
 #' # and dispersion k=0.1, gives the probability of outbreak extinction with a total number
-#' # final outbreak size of exactly 5 to 20 inidividuals (including the initial 5).
+#' # final outbreak size of exactly 5 to 20 individuals (including the initial 5).
 #' pFinalSize(5, 5:20, R=0.2, k=0.1)
 #'
 #' @export
